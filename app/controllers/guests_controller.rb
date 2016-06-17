55dcebb3f2a784cfcabdb1d897fc4ba1fb261f71
lisyk <sergey.liskovoy@gmail.com>
2016-06-16 23:15:30 -0400

    last commit `

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -3,7 +3,47 @@ class GuestsController < ApplicationController
     @guests = Guest.all
   end
 
+  def new
+  end
+
+  def create
+    @guest=Guest.new(name: params[:guest][:name])
+    @guest.save
+    redirect_to guests_path
+  end
+
   def show
     @guest = Guest.find(params[:id])
+  end
+
+  def join
+    @guest=Guest.find(params[:guest_id])
+    if params[:visit] == 'yes'
+      @guest.visit = true
+      @guest.save!
+      respond_to do |format|
+        format.html {
+          redirect_to guest_hooray_path
+        }
+      end
+    else
+      @guest.visit = false
+      @guest.save!
+      respond_to do |format|
+        format.html {
+          redirect_to  guest_upset_path
+        }
+      end
+    end
+
+
+  end
+
+  def hooray
+
+  end
+
+  def upset
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616230010) do
+ActiveRecord::Schema.define(version: 20160617031202) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "visit"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "guests", force: :cascade do |t|
     t.string   "name"
